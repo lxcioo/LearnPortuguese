a415eaa306b2782dd769361c7c78c51412d8ad5b
Lucio Marques Correia <lmcorreia@techfak.uni-bielefeld.de>
2026-05-22 18:46:10 +0200

    10 weitere lektionen

--- src/model/data/content.ts
+++ src/model/data/content.ts
@@ -9,6 +9,16 @@ import unit07 from './units/unit_07_freizeit_hobbys.json';
 import unit08 from './units/unit_08_zuhause.json';
 import unit09 from './units/unit_09_einkaufen_kleidung.json';
 import unit10 from './units/unit_10_körper_gesundheit.json';
+import unit11 from './units/unit_11_arbeit_beruf.json';
+import unit12 from './units/unit_12_technologie.json';
+import unit13 from './units/unit_13_bildung_universitaet.json';
+import unit14 from './units/unit_14_natur_wetter.json';
+import unit15 from './units/unit_15_kultur_traditionen.json';
+import unit16 from './units/unit_16_banken_finanzen.json';
+import unit17 from './units/unit_17_gefuehle_meinungen.json';
+import unit18 from './units/unit_18_medien_nachrichten.json';
+import unit19 from './units/unit_19_fortgeschrittenes_reisen.json';
+import unit20 from './units/unit_20_zukunft_traeume.json';
 
 
 const content = {
@@ -26,7 +36,17 @@ const content = {
         unit07,
         unit08,
         unit09,
-        unit10
+        unit10,
+        unit11,
+        unit12,
+        unit13,
+        unit14,
+        unit15,
+        unit16,
+        unit17,
+        unit18,
+        unit19,
+        unit20
       ]
     }
   ]
